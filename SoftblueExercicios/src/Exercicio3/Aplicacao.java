e7d7c6ed3c02cac8860e7bdc1477cd50dcb44ae3
KyuaKun <vinivsp2304@hotmail.com>
2022-04-22 14:44:07 -0300

    O cálculo do fatorial de um número (!) segue a seguinte regra:Escreva uma aplicação que efetua o cálculo do fatorial utilizando a forma iterativa (usando estruturas de repetição) e outra que efetua o mesmo cálculo, mas de forma recursiva (o método de cálculo do fatorial chama ele mesmo).

---
+++ SoftblueExercicios/src/Exercicio3/Aplicacao.java
@@ -0,0 +1,8 @@
+package Exercicio3;
+
+public class Aplicacao {
+	public static void main(String[] args) {
+		Fatorial fat01 = new Fatorial();
+		System.out.println(fat01.fatorial(6));
+	}
+}
---
+++ SoftblueExercicios/src/Exercicio3/Fatorial.java
@@ -0,0 +1,8 @@
+package Exercicio3;
+
+public class Fatorial {
+	public int fatorial(int num) {
+		if (num == 0) return 1;
+		return num *fatorial(num -1);	
+	}
+}

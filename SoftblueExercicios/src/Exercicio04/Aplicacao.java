9af1565eb930051ad5d90a6b960b1ecac062faca
KyuaKun <vinivsp2304@hotmail.com>
2022-04-22 16:40:25 -0300

    Exercício de Fibonacci usando metodo de com recursividade

---
+++ SoftblueExercicios/src/Exercicio04/Aplicacao.java
@@ -0,0 +1,8 @@
+package Exercicio04;
+
+public class Aplicacao {
+	public static void main(String[] args) {
+		Fibonacci fibonacci = new Fibonacci();
+		System.out.println(fibonacci.fibonacci(13));
+	}
+}

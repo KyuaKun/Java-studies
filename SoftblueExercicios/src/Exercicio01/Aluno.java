209ffa5217f67b86bab3004274f5f1ccf586edec
KyuaKun <vinivsp2304@hotmail.com>
2022-04-22 12:46:33 -0300

    alguns detalhes corrigidos.

--- SoftblueExercicios/src/Exercicio01/Aluno.java
+++ SoftblueExercicios/src/Exercicio01/Aluno.java
@@ -8,17 +8,11 @@ public class Aluno {
 	 final Integer PESO_B1 = 3;
 	 final Integer PESO_B2 = 2;
 	 final Integer PESO_B3 = 5;
-//	 final Double MEDIA_DA_PROVA;
 	
 	public Aluno(String nome, Double notaB1, Double notaB2, Double notaB3) {
 		Aluno.nome = nome;
 		Aluno.notaB1 = notaB1;
 		Aluno.notaB2 = notaB2;
 		Aluno.notaB3 = notaB3;	
-	}
-	
-//	static {
-//		MEDIA_DA_PROVA = (notaB1*3) + (notaB2*2) + (notaB3*5)/3+2+5;
-//	}
-	
+	}	
 }
